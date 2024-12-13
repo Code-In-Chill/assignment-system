3af9b6b161915382899e6c64aea69a897a567b7c
Lê Minh Bảo <besilent308@gmail.com>
2024-12-13 08:50:17 +0700

    Initial commit

---
+++ src/main/java/fis/baolm2/assignmentsystem/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fis.baolm2.assignmentsystem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AssignmentSystemApplication.class);
+    }
+
+}
---
+++ src/test/java/fis/baolm2/assignmentsystem/AssignmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.baolm2.assignmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
