e08c7f55226e6b792f4f1a4a6e421f231bcc1e7f
Lê Minh Bảo <besilent308@gmail.com>
2024-12-14 13:56:21 +0700

    Modify table relationship

--- src/main/java/fis/baolm2/assignmentsystem/entities/Assignment.java
+++ src/main/java/fis/baolm2/assignmentsystem/entities/Assignment.java
@@ -1,5 +1,6 @@
 package fis.baolm2.assignmentsystem.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 @Entity
@@ -15,6 +16,8 @@ public class Assignment {
     private String codeReviewVideoUrl;
 
     @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private User user;
 
     public String getBranch() {
