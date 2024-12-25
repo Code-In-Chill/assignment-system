55a5a1d2414b26010a1ef3fd76ec6d145e732d63
Lê Minh Bảo <besilent308@gmail.com>
2024-12-25 10:30:15 +0700

    Update keycloak client secret

--- src/main/java/fis/baolm2/assignmentsystem/controller/UserController.java
+++ src/main/java/fis/baolm2/assignmentsystem/controller/UserController.java
@@ -1,11 +1,11 @@
 package fis.baolm2.assignmentsystem.controller;
 
 import fis.baolm2.assignmentsystem.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,9 +14,8 @@ public class UserController {
 
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-
 }
